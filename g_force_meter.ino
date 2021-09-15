2656d85b9f7dd5a648235544333c43107a829b45
Luperto Rico <66761413+LupertoRico@users.noreply.github.com>
2021-09-15 09:24:37 -0700

    Update g_force_meter.ino

--- g_force_meter.ino
+++ g_force_meter.ino
@@ -1,151 +1,219 @@
+
+// include TFT and SPI libraries
+#include <TFT.h>  
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
-
-/* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  accel.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" m/s^2");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" m/s^2");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" m/s^2");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
+// pin definition for Arduino UNO
+#define cs   10
+#define dc   9
+#define rst  8
 
-void displayDataRate(void)
-{
-  Serial.print  ("Data Rate:    "); 
-  
-  switch(accel.getDataRate())
-  {
-    case ADXL345_DATARATE_3200_HZ:
-      Serial.print  ("3200 "); 
-      break;
-    case ADXL345_DATARATE_1600_HZ:
-      Serial.print  ("1600 "); 
-      break;
-    case ADXL345_DATARATE_800_HZ:
-      Serial.print  ("800 "); 
-      break;
-    case ADXL345_DATARATE_400_HZ:
-      Serial.print  ("400 "); 
-      break;
-    case ADXL345_DATARATE_200_HZ:
-      Serial.print  ("200 "); 
-      break;
-    case ADXL345_DATARATE_100_HZ:
-      Serial.print  ("100 "); 
-      break;
-    case ADXL345_DATARATE_50_HZ:
-      Serial.print  ("50 "); 
-      break;
-    case ADXL345_DATARATE_25_HZ:
-      Serial.print  ("25 "); 
-      break;
-    case ADXL345_DATARATE_12_5_HZ:
-      Serial.print  ("12.5 "); 
-      break;
-    case ADXL345_DATARATE_6_25HZ:
-      Serial.print  ("6.25 "); 
-      break;
-    case ADXL345_DATARATE_3_13_HZ:
-      Serial.print  ("3.13 "); 
-      break;
-    case ADXL345_DATARATE_1_56_HZ:
-      Serial.print  ("1.56 "); 
-      break;
-    case ADXL345_DATARATE_0_78_HZ:
-      Serial.print  ("0.78 "); 
-      break;
-    case ADXL345_DATARATE_0_39_HZ:
-      Serial.print  ("0.39 "); 
-      break;
-    case ADXL345_DATARATE_0_20_HZ:
-      Serial.print  ("0.20 "); 
-      break;
-    case ADXL345_DATARATE_0_10_HZ:
-      Serial.print  ("0.10 "); 
-      break;
-    default:
-      Serial.print  ("???? "); 
-      break;
-  }  
-  Serial.println(" Hz");  
-}
+// create an instance of the library
+TFT TFTscreen = TFT(cs, dc, rst);
 
-void displayRange(void)
-{
-  Serial.print  ("Range:         +/- "); 
-  
-  switch(accel.getRange())
-  {
-    case ADXL345_RANGE_16_G:
-      Serial.print  ("16 "); 
-      break;
-    case ADXL345_RANGE_8_G:
-      Serial.print  ("8 "); 
-      break;
-    case ADXL345_RANGE_4_G:
-      Serial.print  ("4 "); 
-      break;
-    case ADXL345_RANGE_2_G:
-      Serial.print  ("2 "); 
-      break;
-    default:
-      Serial.print  ("?? "); 
-      break;
-  }  
-  Serial.println(" g");  
-}
+int x = 60;
+int y = TFTscreen.height()/2;
 
-void setup(void) 
-{
+int xPos;
+int yPos;
+
+int xPrev = x;
+int yPrev = y;
+
+float gforce = 0.00;
+float gforce_prev = gforce;
+
+
+void setup() {
   Serial.begin(9600);
   Serial.println("Accelerometer Test"); Serial.println("");
-  
-  /* Initialise the sensor */
+  accel.setRange(ADXL345_RANGE_2_G);
+  // Initialise the sensor 
   if(!accel.begin())
   {
-    /* There was a problem detecting the ADXL345 ... check your connections */
+    // There was a problem detecting the ADXL345 ... check your connections 
     Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
     while(1);
   }
-  /* Set the range to whatever is appropriate for your project */
-  accel.setRange(ADXL345_RANGE_2_G);
-  // displaySetRange(ADXL345_RANGE_8_G);
-  // displaySetRange(ADXL345_RANGE_4_G);
-  // displaySetRange(ADXL345_RANGE_2_G);
   
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
+  //initialize the library
+  TFTscreen.begin();
+
+  //start up 
+  printFlag();
+  welcome();
+  
+  // clear the screen with a black background
+  TFTscreen.background(0, 0, 0);  
+  TFTscreen.stroke(0, 150, 0);
+  TFTscreen.circle(60,TFTscreen.height()/2,55);
+  TFTscreen.circle(60,TFTscreen.height()/2,27);
+  TFTscreen.line(60,4,60,125);
+  TFTscreen.line(0,TFTscreen.height()/2,121,TFTscreen.height()/2);
+
+  TFTscreen.setTextSize(1);
+  TFTscreen.text("<- ->",120,87);
 }
 
-void loop(void) 
+void welcome()
 {
-  /* Get a new sensor event */ 
+  int color = 0;
+  TFTscreen.background(0,0,0);
+
+  delay(200);
+  while(color < 255)
+  {
+    TFTscreen.stroke(color,color,color);
+    TFTscreen.setTextSize(3);
+    TFTscreen.text("WELCOME,",5,40);
+    TFTscreen.text("ABOARD!",5,80);
+    color = color + 5;
+  }
+  delay(500);
+  
+  while(color > 0){
+    TFTscreen.stroke(color,color,color);
+    TFTscreen.setTextSize(3);
+    TFTscreen.text("WELCOME,",5,40);
+    TFTscreen.text("ABOARD!",5,80);
+    color = color - 20;
+  }
+  TFTscreen.noStroke();
+  
+}
+
+void printFlag()
+{
+  TFTscreen.background(0,0,255);
+  TFTscreen.stroke(0,255,0);
+  TFTscreen.fill(0,255,0);
+  TFTscreen.rect(0,0,53,128);
+
+  TFTscreen.stroke(255,255,255);
+  TFTscreen.fill(255,255,255);
+  TFTscreen.rect(53,0,53,128);
+
+  TFTscreen.noStroke();
+  TFTscreen.noFill();
+  delay(1500);
+}
+
+void loop() {
   sensors_event_t event; 
   accel.getEvent(&event);
   
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("   \t");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("   \t");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("   \t");Serial.println("m/s^2 ");
-
-
-  //Serial.print(event.acceleration.z*0.101971621);
+  TFTscreen.stroke(0, 150, 0);
+  TFTscreen.circle(60,TFTscreen.height()/2,55);
+  TFTscreen.circle(60,TFTscreen.height()/2,27);
+  TFTscreen.line(60,4,60,125);
+  TFTscreen.line(0,TFTscreen.height()/2,121,TFTscreen.height()/2);
   
-  delay(24);
+  //event.acceleration.x*0.101971621
+  //event.acceleration.y*0.101971621
+  //27 is 50%,55 is 100%
+  
+  x = 60;
+  y = TFTscreen.height()/2;
+
+  x = x - (event.acceleration.x*0.101971621)*55;
+  y = y - (event.acceleration.y*0.101971621)*55;
+
+  xPos = y;
+  yPos = x;
+
+  if(xPos != xPrev or yPos != yPrev)
+  {
+    TFTscreen.fill(0,0,0);
+    TFTscreen.stroke(0,0,0);
+    TFTscreen.circle(xPrev,yPrev,4);  
+    TFTscreen.noFill();
+    TFTscreen.noStroke();    
+  }  
+  
+  TFTscreen.fill(0,150,0);
+  TFTscreen.circle(xPos,yPos,4);
+  TFTscreen.noFill();
+
+  xPrev = xPos;
+  yPrev = yPos;  
+
+  //gforce data
+  
+  
+  //double xforce = event.acceleration.x*0.101971621;
+  double yforce = event.acceleration.y*0.101971621 - .03;
+
+  /*
+  if(xforce < 0)
+  {
+    xforce = -xforce;  
+  }
+  */
+
+  if(yforce < 0)
+  {
+    yforce = -yforce;  
+  }
+  
+  gforce = yforce;
+  
+  if(gforce > 1)
+  {
+    gforce = 1.00;  
+  }
+  
+  TFTscreen.setTextSize(2);
+  if(gforce != gforce_prev)
+  {
+   TFTscreen.setCursor(110,100); 
+   TFTscreen.stroke(0,0,0);
+   TFTscreen.print(gforce_prev);
+  }
+
+  
+  if(gforce > .7)
+  {
+    TFTscreen.stroke(0,0,225);  
+  }
+  else if(gforce > .5)
+  { 
+    TFTscreen.stroke(0,100,220);
+  }
+  else if(gforce > .35)
+  {
+    TFTscreen.stroke(0,150,200);
+  }
+  else
+  {
+    TFTscreen.stroke(0,150,0);
+  }
+  
+  TFTscreen.setCursor(110,100);
+  
+  TFTscreen.print(gforce);
+
+  gforce_prev = gforce;
+  //gforce data end
+  //delay(2);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
